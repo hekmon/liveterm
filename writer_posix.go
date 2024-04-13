0ee43536fec2d7a34efdc5fe52d3e56c81505e2a
Hekmon <3418467+hekmon@users.noreply.github.com>
2024-04-13 22:54:50 +0200

    enhance source code readability

--- writer_posix.go
+++ writer_posix.go
@@ -7,9 +7,13 @@ import (
 	"strings"
 )
 
-// clear the line and move the cursor up
-var clear = fmt.Sprintf("%c[%dA%c[2K", esc, 1, esc)
+// https://shiroyasha.svbtle.com/escape-sequences-a-quick-guide-1
+var (
+	cursorUP          = fmt.Sprintf("%c[1A", esc)
+	clearLine         = fmt.Sprintf("%c[2K", esc)
+	clearPreviousLine = cursorUP + clearLine
+)
 
 func clearLines() {
-	_, _ = fmt.Fprint(out, strings.Repeat(clear, lineCount))
+	_, _ = fmt.Fprint(out, strings.Repeat(clearPreviousLine, lineCount))
 }
