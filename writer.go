a1051534957e90d8c3d60c36db1873dc8b3eef95
Hekmon <3418467+hekmon@users.noreply.github.com>
2024-04-14 11:00:29 +0200

    organize files

--- writer.go
+++ writer.go
@@ -2,132 +2,17 @@ package termlive
 
 import (
 	"bytes"
-	"fmt"
+	"errors"
 	"io"
-	"os"
-	"sync"
-	"time"
 )
 
 // ESC is the ASCII code for escape character
 const esc = 27
 
 var (
-	// Config
-	RefreshInterval = 100 * time.Millisecond // RefreshInterval is the default refresh interval to update the ui
-	UseStdErr       = false                  // use StdErr instead of StdOut
-	// Internal
-	out         io.Writer
-	ticker      *time.Ticker
-	tdone       chan bool
-	getterLines func() []string
-	getterLine  func() string
-	getterRaw   func() []byte
-	buf         bytes.Buffer
-	lineCount   int
-	mtx         sync.Mutex
+	buf       bytes.Buffer
+	lineCount int
 )
-
-// SetMultiLinesDataFx sets the function that returns the data to be displayed in the terminal.
-// There is no need to end each line with a '\n' as it will be added automatically.
-func SetMultiLinesUpdateFx(fx func() []string) {
-	mtx.Lock()
-	getterLines = fx
-	getterLine = nil
-	getterRaw = nil
-	mtx.Unlock()
-}
-
-// SetMultiLinesDataFx sets the function that returns the data to be displayed in the terminal.
-// There is no need to end each line with a '\n' as it will be added automatically.
-func SetSingleLineUpdateFx(fx func() string) {
-	mtx.Lock()
-	getterLines = nil
-	getterLine = fx
-	getterRaw = nil
-	mtx.Unlock()
-}
-
-// SetMultiLinesDataFx sets the function that returns the data to be displayed in the terminal.
-// You are responsible for adding the trailing '\n' at the end.
-func SetRawUpdateFx(fx func() []byte) {
-	mtx.Lock()
-	getterLines = nil
-	getterLine = nil
-	getterRaw = fx
-	mtx.Unlock()
-}
-
-// Start starts the updater in a non-blocking manner.
-// After calling Start(), the output (stdout or stderr) should not be used directly anymore.
-func Start() {
-	defer mtx.Unlock()
-	mtx.Lock()
-	// Nullify multiples calls to start
-	if ticker != nil {
-		return
-	}
-	// Start the updater
-	if UseStdErr {
-		out = os.Stderr
-	} else {
-		out = os.Stdout
-	}
-	ticker = time.NewTicker(RefreshInterval)
-	tdone = make(chan bool)
-	startListeningForTermResize()
-	go worker()
-}
-
-// Stop stops the updater that updates the terminal. Clear will erase dynamic data from the terminal before stopping.
-// Choosen output (stdout or stderr) can be used again directly after this call.
-func Stop(clear bool) {
-	tdone <- clear
-	<-tdone
-}
-
-// ForceUpdate forces an update of the terminal even if out of tick
-func ForceUpdate() {
-	mtx.Lock()
-	update()
-	mtx.Unlock()
-}
-
-func worker() {
-	var clear bool
-	for {
-		select {
-		case <-ticker.C:
-			mtx.Lock()
-			update()
-			mtx.Unlock()
-		case <-termSizeChan:
-			mtx.Lock()
-			termSize = getTermSize()
-			mtx.Unlock()
-		case clear = <-tdone:
-			mtx.Lock()
-			ticker.Stop()
-			ticker = nil
-			stopListeningForTermResize()
-			if clear {
-				erase()
-			} else {
-				// update ui one last time with latest possible data
-				update()
-			}
-			out = nil
-			getterLines = nil
-			getterLine = nil
-			getterRaw = nil
-			buf.Reset()
-			lineCount = 0
-			close(tdone)
-			mtx.Unlock()
-			return
-		}
-	}
-}
 
 // update is unsafe ! It must be called within a mutex lock by one of its callers
 func update() {
@@ -197,7 +82,7 @@ func (bypass) Write(p []byte) (n int, err error) {
 	mtx.Lock()
 	// if termlive is not started, out is nil
 	if out == nil {
-		err = fmt.Errorf("termlive is not started, can not write to terminal")
+		err = errors.New("termlive is not started, can not write to terminal")
 		return
 	}
 	// erase current dynamic data
